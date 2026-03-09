0798b3d5b5d7865c3476ac082815174509abbd57
jungmin <kjmhs501@gmail.com>
2026-03-09 16:55:50 +0900

    feat: 로그인 버튼 헤더에 추가

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div />;
 };
 
 export default Login;
--- src/shared/layouts/header-gnb.tsx
+++ src/shared/layouts/header-gnb.tsx
@@ -1,16 +1,24 @@
+import MainLogo from '@assets/icons/logo.svg?react';
+import Button from '@components/button';
 import { useNavigate } from 'react-router-dom';
-import MainLogo from "@assets/icons/logo.svg?react";
 
 const HeaderGnb = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex-row-between px-[4rem] py-[1.3rem] border-b border-gray-10">
-      <button type='button' className="cursor-pointer" onClick={() => navigate('/')}>
-        <MainLogo className="w-[4.3rem] h-[3.8rem]"/>
+    <div className="flex-row-between border-gray-10 border-b px-[4rem] py-[1.3rem]">
+      <button type="button" className="cursor-pointer" onClick={() => navigate('/')}>
+        <MainLogo className="h-[3.8rem] w-[4.3rem]" />
       </button>
 
-      <button>로그인</button>
+      <Button
+        variant="primary_outline"
+        size="sm"
+        className="h-[3.2rem] w-[8rem]"
+        onClick={() => navigate('/login')}
+      >
+        로그인
+      </Button>
     </div>
   );
 };
