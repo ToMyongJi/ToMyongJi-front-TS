a530c1c395d0d86b7936b959253f071e15e84fac
choikyungsoo <ryan1954@naver.com>
2026-03-12 16:45:18 +0900

    feat: 커스텀 페이지네이션 컴포넌트 제작

---
+++ src/shared/components/pagination-custom.tsx
@@ -0,0 +1,85 @@
+import {cn} from '@libs/cn';
+import { ComponentType, SVGProps } from "react";
+import ArrowLeftIcon from "@assets/icons/arrow-left.svg?react";
+import ArrowRightIcon from "@assets/icons/arrow-right.svg?react";
+
+interface PaginationButtonProps {
+  isActive: boolean;
+  content: number;
+  onClick: () => void;
+}
+
+interface PaginationIconButtonProps {
+  isDisabled: boolean;
+  Icon: ComponentType<SVGProps<SVGSVGElement>>;
+  onClick?: () => void;
+}
+
+interface PaginationProps  {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (pageNumber: number) => void;
+}
+
+const PaginationButton = ({isActive, content, onClick}: PaginationButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn('W_SB14 w-[3rem] cursor-pointer rounded-full p-[0.7rem] text-gray-70', isActive && "bg-background text-black")}
+    >
+      {content}
+    </button>
+  )
+}
+
+const PaginationIconButton = ({isDisabled, Icon, onClick} : PaginationIconButtonProps) => {
+  return (
+    <button
+      type="button"
+      disabled={isDisabled}
+      onClick={onClick}
+      className={cn('cursor-pointer', isDisabled && 'text-gray-20')}>
+      <Icon />
+    </button>
+  )
+}
+
+const PaginationCustom = ({ currentPage, totalPages, onPageChange }: PaginationProps) => {
+  const pageNumbers = [];
+  const maxPageButtons = 5;
+
+  let startPage = Math.max(1, currentPage - Math.floor(maxPageButtons / 2));
+  const endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
+
+  if (endPage - startPage + 1 < maxPageButtons) {
+    startPage = Math.max(1, endPage - maxPageButtons + 1);
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="flex-row-center gap-[0.4rem]">
+      <PaginationIconButton
+        isDisabled={currentPage === 1}
+        onClick={() => onPageChange(currentPage - 1)}
+        Icon={ArrowLeftIcon}/>
+      {pageNumbers.map((pageNumber) => (
+        <PaginationButton
+          key={pageNumber}
+          onClick={() => onPageChange(pageNumber)}
+          isActive={pageNumber === currentPage}
+          content={pageNumber}
+         />
+      ))}
+      <PaginationIconButton
+        isDisabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+        Icon={ArrowRightIcon}/>
+    </div>
+  );
+};
+
+export default PaginationCustom;
