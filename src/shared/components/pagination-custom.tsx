77a9e00a7461e652a09858d0df413db9297b6917
choikyungsoo <ryan1954@naver.com>
2026-03-17 13:44:00 +0900

    fix: 데이터가 없을 경우 페이지네이션 0으로 처리

--- src/shared/components/pagination-custom.tsx
+++ src/shared/components/pagination-custom.tsx
@@ -46,36 +46,40 @@ const PaginationIconButton = ({isDisabled, Icon, onClick} : PaginationIconButton
 }
 
 const PaginationCustom = ({ currentPage, totalPages, onPageChange }: PaginationProps) => {
-  const pageNumbers = [];
+  const pageNumbers: number[] = [];
   const maxPageButtons = 5;
 
-  let startPage = Math.max(1, currentPage - Math.floor(maxPageButtons / 2));
-  const endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
+  if (totalPages === 0) {
+    pageNumbers.push(0);
+  } else {
+    let startPage = Math.max(1, currentPage - Math.floor(maxPageButtons / 2));
+    const endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
 
-  if (endPage - startPage + 1 < maxPageButtons) {
-    startPage = Math.max(1, endPage - maxPageButtons + 1);
-  }
+    if (endPage - startPage + 1 < maxPageButtons) {
+      startPage = Math.max(1, endPage - maxPageButtons + 1);
+    }
 
-  for (let i = startPage; i <= endPage; i++) {
-    pageNumbers.push(i);
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(i);
+    }
   }
 
   return (
     <div className="flex-row-center gap-[0.4rem]">
       <PaginationIconButton
-        isDisabled={currentPage === 1}
+        isDisabled={currentPage === 1 || totalPages === 0}
         onClick={() => onPageChange(currentPage - 1)}
         Icon={ArrowLeftIcon}/>
       {pageNumbers.map((pageNumber) => (
         <PaginationButton
           key={pageNumber}
-          onClick={() => onPageChange(pageNumber)}
+          onClick={() => totalPages > 0 && onPageChange(pageNumber)}
           isActive={pageNumber === currentPage}
           content={pageNumber}
          />
       ))}
       <PaginationIconButton
-        isDisabled={currentPage === totalPages}
+        isDisabled={currentPage === totalPages || totalPages === 0}
         onClick={() => onPageChange(currentPage + 1)}
         Icon={ArrowRightIcon}/>
     </div>
