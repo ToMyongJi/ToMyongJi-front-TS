cb2033c141de69005534a3fcef6bf85f52fd3a40
jungmin <kjmhs501@gmail.com>
2026-03-26 14:50:18 +0900

    fix: vite build할 때 각 폴더에 js파일 생기는 문제 해결

--- src/shared/apis/my/my-queries.js
+++
@@ -1,7 +0,0 @@
-import { buildQuery } from '@apis/base/factory';
-import { QK } from '@apis/base/key';
-import { myApi } from './my';
-export const myQuery = {
-    getMyInfo: (id) => buildQuery(QK.my.getMyInfo(id), () => myApi.getMyInfo(id)),
-    viewMember: (id) => buildQuery(QK.my.viewMember(id), () => myApi.viewMember(id)),
-};
--- src/shared/apis/receipt/receipt.js
+++
@@ -1,7 +0,0 @@
-import { HttpClient } from '@apis/base/http';
-import { axiosInstance } from '@apis/base/instance';
-import { ENDPOINTS } from '@apis/constants/endpoints';
-const http = new HttpClient(axiosInstance);
-export const receiptApi = {
-    list: (clubId, params) => http.get(ENDPOINTS.receipt.paging(clubId), { params })
-};
